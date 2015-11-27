2744e49234d1a3e7d0b8d9f91627c494c2f324ac
Ryan Hamblin <ryanhca@gmail.com>
2015-11-27 10:59:13 -0600

    got clock to start ticking

---
+++ clock.js
@@ -0,0 +1,41 @@
+function endDate(end) {
+  var t = Date.parse(end) - Date.parse(new Date());
+  var seconds = Math.floor((t / 1000) % 60);
+  var minutes = Math.floor((t / 1000 / 60) % 60);
+  var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+  var days = Math.floor(t / (1000 * 60 * 60 * 24));
+  return {
+    'total': t,
+    'days': days,
+    'hours': hours,
+    'minutes': minutes,
+    'seconds': seconds
+  };
+}
+
+function startClock(id, end) {
+  var clock = document.getElementById(id);
+  var daysSpan = clock.querySelector('.days');
+  var hoursSpan = clock.querySelector('.hours');
+  var minutesSpan = clock.querySelector('.minutes');
+  var secondsSpan = clock.querySelector('.seconds');
+
+  function countDown() {
+    var t = endDate(end);
+
+    daysSpan.innerHTML = t.days;
+    hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+    minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+    secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+
+    if (t.total <= 0) {
+      clearInterval(timeinterval);
+    }
+  }
+
+  countDown();
+  var timeinterval = setInterval(countDown, 1000);
+}
+
+var deadline = 'December 18 2015 00:00:50 UTC+0200';
+startClock('clockdiv', deadline);
